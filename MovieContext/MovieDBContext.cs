d15a1fbdae71691fd5a95f20b9b4f2dd092dc73f
Aowal052 <azadaowal@gmail.com>
2019-10-03 02:06:39 +0600

    Its a crud repositury system,To create it i use .Net MVC and Entity Framework

---
+++ MovieContext/MovieDBContext.cs
@@ -0,0 +1,26 @@
+﻿using NewApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+
+namespace NewApp.MovieContext
+{
+    public class MovieDBContext: DbContext
+    {
+        public MovieDBContext() : base("MovieDb")
+        {
+
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+        }
+    }
+}
